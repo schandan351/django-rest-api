5d54f47acc5d4ca7f2fef2b55f24f6c551b7b23b
chandan sharma <sharmachandan351@gmail.com>
2018-12-19 17:42:27 +0545

    created search filter using name and email

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from .serializers import HelloSerializer,UserProfileSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from . import models
 from .import permissions
 # Create your views here.
@@ -88,4 +89,7 @@ class UserProfileViewset(viewsets.ModelViewSet):
     queryset=models.UserProfile.objects.all()
     authentication_classes=(TokenAuthentication,)
     permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email',)
+
 
