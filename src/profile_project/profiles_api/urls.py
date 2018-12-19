14ea74eac05b2a590c971e1e8c4c827669369905
chandan sharma <sharmachandan351@gmail.com>
2018-12-19 15:27:42 +0545

    successfully created put post patch and delete

---
+++ src/profile_project/profiles_api/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from .views import HelloApiView
+urlpatterns=[
+    url(r'^hello-view/',HelloApiView.as_view()),
+]
--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import HelloSerializer
 # Create your views here.
+
+
+class HelloApiView(APIView):
+
+    serialize_class=HelloSerializer
+    def get(self,request,format=None):
+        an_apiview=[
+            'uses https method  as a function',
+            'helloocdsnj',
+            'fukcing awesome',
+            'lauda ',
+        ]
+        return Response({'message':'Hello','an_apiview':an_apiview})
+
+
+    def post(self,request):
+        serializer=HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.data.get('name')
+            message='hello{0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk=None):
+        return Response({'method':'put'})
+
+    def patch(self,request,pk=None):
+        return Response({'method':'patch'})
+    
+    def delete(self,request,pk=None):
+        return Response({'method':'delete'})
